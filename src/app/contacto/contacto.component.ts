da01b7f30ff9edfd04e123d9354c410e34bd8835
EstebanM159 <estebanm91231@gmail.com>
2023-10-19 07:31:38 -0300

    Se agregaron algunas validaciones al formulario de contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ContactoClass } from '../models/contacto';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  clienteContacto:ContactoClass=new ContactoClass;
   open:boolean=false;
   telefono(){
     this.open=true;
   }
   email(){
     this.open=false
+  }
+  a(){
+    console.log(this.clienteContacto);
   }
 }
---
+++ src/app/models/contacto.ts
@@ -0,0 +1,6 @@
+  export class ContactoClass {
+  public nombre!:string;
+  public email!: string;
+  public tel !: number;
+  public mensaje !:string;
+  }
